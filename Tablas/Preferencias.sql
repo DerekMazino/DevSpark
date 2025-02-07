7fb5e134a480c4db2659bbe373384842f7183813
Camilo Marín <juancmaringcu@gmail.com>
2025-02-07 17:48:59 -0500

    Se ajusta llave foranea de preferencias.

--- Tablas/Preferencias.sql
+++ Tablas/Preferencias.sql
@@ -9,7 +9,7 @@ CREATE TABLE Preferencias (
     id_cliente INT NOT NULL,
     id_producto INT NOT NULL,
     recomendacion VARCHAR(30) CHECK (recomendacion IN ('Altamente Recomendado', 'Recomendado', 'No Recomendado')),
-    CONSTRAINT fk_cliente FOREIGN KEY (id_cliente) REFERENCES Preferencias(id_cliente) ON DELETE CASCADE,
+    CONSTRAINT fk_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE,
     CONSTRAINT fk_producto FOREIGN KEY (id_producto) REFERENCES productos(id_producto) ON DELETE CASCADE
 );
 
