4832b651a4d10dd6a05dbeaa5459d4768485160a
Camilo Marín <juancmaringcu@gmail.com>
2025-02-07 08:09:44 -0500

    Se ajusta P_ACTUALIZA_STOCK para que no se almacene stock negativo. Se agrega comments a la tabla clientes.

--- Procedimientos/P_ACTUALIZA_STOCK.sql
+++ Procedimientos/P_ACTUALIZA_STOCK.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE PROCEDURE P_ACTUALIZA_STOCK(producto_id INT, nueva_cantidad IN
 AS $$
 BEGIN
     UPDATE Productos
-    SET stock = stock + nueva_cantidad
+    SET stock = GREATEST(stock + nueva_cantidad, 0)
     WHERE id_producto = producto_id;
 END;
 $$ LANGUAGE plpgsql;
